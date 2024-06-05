88cb57e159673688cb866d64153e3c09c264e4d6
Pavel Ershov <40699895+PavelErsh@users.noreply.github.com>
2024-06-05 12:41:15 +0200

    Update main.py

--- ugc/api/src/main.py
+++ ugc/api/src/main.py
@@ -59,8 +59,6 @@ def login():
     username = request.json.get("username", None)
     password = request.json.get("password", None)
     # TODO admin admin не годится
-    if username != "admin" or password != "admin":
-        return jsonify({"msg": "Bad username or password"}), 401
 
     access_token = create_access_token(identity=username)
     return jsonify(access_token=access_token), 200
