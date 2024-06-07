3a7a4201a244f0c05b03e633fd3ba8d2fd033d50
Andrei Mihailov <andrew19220@yandex.ru>
2024-06-07 21:34:36 +0300

    fix auth

--- ugc/api/utils/auth.py
+++ ugc/api/utils/auth.py
@@ -1,7 +1,14 @@
-import jwt
-from werkzeug.exceptions import Unauthorized, Forbidden
-from core.config import settings
 import requests
+import jwt
+import aiohttp
+
+from werkzeug.exceptions import Unauthorized, Forbidden
+from fastapi import status, HTTPException, Request
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from typing import Optional
+
+from api.src.core.config import settings
+
 
 def decode_jwt(
     jwt_token: str,
@@ -20,11 +27,66 @@ def decode_jwt(
         raise Unauthorized(description="Срок действия токена истек, обновите токен")
     return decoded
 
+
 def parse_token(jwt_token: str) -> dict:
     return decode_jwt(jwt_token)
+
 
 def check_user(headers: dict) -> bool:
     response = requests.get("http://127.0.0.1:8080/api/v1/users/me", headers=headers)
     if response.status_code != 202:
         raise Forbidden(description="Пользователь не существует")
     return True
+
+
+class JWTBearer(HTTPBearer):
+    def __init__(self, check_user: bool = False, auto_error: bool = True):
+        super().__init__(auto_error=auto_error)
+        self.check_user = check_user
+
+    async def __call__(self, request: Request) -> dict:
+        credentials: HTTPAuthorizationCredentials = await super().__call__(request)
+        if not credentials:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Invalid authorization code.",
+            )
+        if not credentials.scheme == "Bearer":
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Only Bearer token might be accepted",
+            )
+        decoded_token = self.parse_token(credentials.credentials)
+        if not decoded_token:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Invalid or expired token.",
+            )
+
+        if self.check_user:
+            # проверить usera в бд
+            response = await self.check(
+                "http://127.0.0.1:8080/api/v1/users/me",
+                params={},
+                headers={"Authorization": f"Bearer {credentials.credentials}"},
+            )
+            if response.status != status.HTTP_202_ACCEPTED:
+                raise HTTPException(
+                    status_code=status.HTTP_403_FORBIDDEN, detail="User doesn't exist"
+                )
+
+        return decoded_token
+
+    @staticmethod
+    def parse_token(jwt_token: str) -> Optional[dict]:
+        return decode_jwt(jwt_token=jwt_token)
+
+    @staticmethod
+    async def check(query: str, params: dict = {}, headers: dict = {}, json: dict = {}):
+        async with aiohttp.ClientSession(headers=headers) as client:
+            response = await client.get(query, json=json, params=params)
+        return response
+
+
+security_jwt = JWTBearer()
+security_jwt_check = JWTBearer(check_user=True)
